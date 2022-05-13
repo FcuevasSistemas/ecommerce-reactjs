0d252ad808a5f0ae4b161f420ec8ba5b8e5b419f
Federico <fcuevas.sistemas@gmail.com>
2022-05-13 15:41:27 -0300

    navBar Aplicado

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+const NavBar = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar>
+            <Toolbar>
+                <div className="menubtn">
+                    <Button color="inherit" className="btn1">Productos</Button>
+                    <Button color="inherit" className="btn2">Historia</Button>
+                </div>
+                <div className="navlogo">
+                    <img src="./cucusneakerslogo.png" alt=""/>
+                </div>
+                <Button color="inherit">
+                    <ShoppingBasketIcon/>
+                </Button>
+            </Toolbar>
+        </AppBar>
+        </Box>
+    );
+    };
+
+export default NavBar;
