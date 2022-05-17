0cee402037c886ba18a3bd30259ce2356130db08
Federico <fcuevas.sistemas@gmail.com>
2022-05-16 23:02:57 -0300

    titulo por prop Item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
-import CardProduct from './components/Card/Card';
+//import CardProduct from './components/Card/Card';
+import ItemListContainer from './components/ItemListContainer/ItemListConatiner';
 
 
 function App() {
   return (
     <div className="App">
         <NavBar />
-        <CardProduct producttitle={"Sneakers"}/>
+        <ItemListContainer greeting={"Sneakers"}/>
       
   </div>
   );
--- src/components/Item/Card.js
+++ src/components/Item/Card.js
@@ -4,11 +4,11 @@ import CardContent from '@mui/material/CardContent';
 import { Button } from '@mui/material';
 
 const CardProduct = (data) => {
-    const { image, title, price, producttitle } = data
+    const { image, title, price,} = data
 return (
     <Card>
         <CardContent>
-        <h1>{producttitle}</h1>
+        
 
         <Button variant="contained" color="success" className='btncompra'>Comprar</Button>
     </CardContent>
